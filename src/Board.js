df5375367c1ea9c25d9a63029b426d402a0f44f7
programarivm <info@programarivm.com>
2023-10-18 15:40:23 +0200

    Removed BoardAudio

--- src/Board.js
+++ src/Board.js
@@ -1,14 +1,8 @@
 import ActiveSquares from './ActiveSquares';
-import ResizeSlider from './ResizeSlider';
-import BoardAudio from './BoardAudio';
 
 const Board = () => {
   return (
-    <>
-      <ActiveSquares />
-      <ResizeSlider />
-      <BoardAudio />
-    </>
+    <ActiveSquares />
   );
 }
 

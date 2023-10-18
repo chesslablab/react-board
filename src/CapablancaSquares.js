e7ff36335dacb600b2379162c874617d6778e173
programarivm <info@programarivm.com>
2023-10-18 17:06:09 +0200

    Updated CapablancaSquares

--- src/CapablancaSquares.js
+++ src/CapablancaSquares.js
@@ -1,29 +1,15 @@
 import { useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Piece from 'common/Piece';
-import * as board from 'features/board/boardSlice';
-import Squares from 'features/board/Squares';
-import Ws from 'features/ws/Ws';
+import Squares from './Squares';
 
 const CapablancaSquares = () => {
-  const state = useSelector(state => state.board);
-
-  const dispatch = useDispatch();
-
   return (
-    <Squares props={{
-      className: 'capablancaSquares',
-      imgsRef:  useRef([]),
-      sqsRef: useRef([]),
-      handleMove: (payload) => {
-        if (state.turn === Piece.color(payload.piece)) {
-          dispatch(board.grabPiece(payload));
-          Ws.legal(payload.sq);
-        } else {
-          dispatch(board.placePiece(payload));
-        }
-      }
-    }} />
+    <Squares
+      props={{
+        className: 'capablancaSquares',
+        imgsRef:  useRef([]),
+        sqsRef: useRef([]),
+      }}
+    />
   );
 }
 

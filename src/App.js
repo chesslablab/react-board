6f3c5accda30ccc9547248bdc86793a9dc881e01
programarivm <info@programarivm.com>
2023-10-19 08:58:42 +0200

    Updated App

--- src/App.js
+++ src/App.js
@@ -26,21 +26,30 @@ function App() {
   const grabPiece = (payload) => {
     const fen = state.fen[state.fen.length - 1].split(' ');
     const ascii = Ascii.toAscii(fen[0]);
-    setState({
-      ...state,
-      ...{
-        lan: payload.sq,
-        pieceGrabbed: {
-          i: payload.i,
-          j: payload.j,
-          sq: payload.sq,
-          ascii: ascii[payload.i][payload.j]
-        }
-      }});
+    const newState = Object.assign({}, state);
+    newState.lan = payload.sq;
+    newState.pieceGrabbed = {
+      i: payload.i,
+      j: payload.j,
+      sq: payload.sq,
+      ascii: ascii[payload.i][payload.j],
+    };
+    setState(newState);
   };
 
   const placePiece = (payload) => {
-    // TODO
+    if (state.pieceGrabbed) {
+      const newState = Object.assign({}, state);
+      if (state.pieceGrabbed.ascii === ' . ') {
+        delete newState.pieceGrabbed;
+      } else {
+        newState.lan += payload.sq;
+        newState.turn = newState.turn === Pgn.symbol.WHITE ? Pgn.symbol.BLACK : Pgn.symbol.WHITE;
+        newState.piecePlaced = payload.piecePlaced;
+        delete newState.pieceGrabbed;
+      }
+      setState(newState);
+    }
   };
 
   const filterMove = () => {

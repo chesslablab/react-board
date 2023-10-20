c9d53e37d913dfcab1e6f813f7261434022aeba3
programarivm <info@programarivm.com>
2023-10-20 14:15:19 +0200

    Implemented onMouseUp()

--- src/Squares.js
+++ src/Squares.js
@@ -14,11 +14,6 @@ const Squares = ({
     console.log('mouse move!');
   }
 
-  const handleMouseUp = (event) => {
-    // TODO
-    console.log('mouse up!');
-  }
-
   const sqs = () => {
     const fen = props.fen[props.fen.length - 1].split(' ');
     const ascii = Ascii.toAscii(fen[0]);
@@ -88,7 +83,7 @@ const Squares = ({
               handleMove(payload);
             }
           }}
-          onDrop={(ev) => {
+          onMouseUp={(ev) => {
             ev.preventDefault();
             if (filterMove()) {
               payload.piecePlaced = {
@@ -118,7 +113,6 @@ const Squares = ({
     <div
       className={[props.className].join(' ')}
       onMouseMove={handleMouseMove}
-      onMouseUp={handleMouseUp}
     >
       {sqs()}
     </div>

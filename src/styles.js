68f6f09acf5b318e8ccf14f5f01d16222b990c7f
programarivm <info@programarivm.com>
2023-10-25 19:53:48 +0200

    Improved styles

--- src/styles.js
+++ src/styles.js
@@ -69,7 +69,7 @@ const styles = {
     color: '#ffce9e',
   },
   slider:{
-    width: "98%",
+    width: "100%",
     background: "#d3d3d3",
     WebkitTransition: ".2s",
     transition: "opacity .2s",

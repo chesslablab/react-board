adb21ca20ee863bb2c68b1c18ef01c3bbe69a205
programarivm <info@programarivm.com>
2023-10-27 10:53:04 +0200

    Styled buttons

--- src/HistoryButtons.js
+++ src/HistoryButtons.js
@@ -10,24 +10,28 @@ export const HistoryButtons = ({
   return (
     <div style={styles}>
       <button
+        style={styles.button}
         disabled={stateHistoryButtons.fen.length - 1 - Math.abs(stateHistoryButtons.back) === 0}
         onClick={() => onFastRewindClick()}
       >
         ⏮
       </button>
       <button
+        style={styles.button}
         disabled={stateHistoryButtons.fen.length - 1 - Math.abs(stateHistoryButtons.back) === 0}
         onClick={() => onSkipPreviousClick()}
       >
         ⏪
       </button>
       <button
+        style={styles.button}
         disabled={stateHistoryButtons.back === 0}
         onClick={() => onSkipNextClick()}
       >
         ⏩
       </button>
       <button
+        style={styles.button}
         disabled={stateHistoryButtons.back === 0}
         onClick={() => onFastForwardClick()}
       >

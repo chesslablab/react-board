97d9fb3a163bc46306a4c7630452732fb0502267
programarivm <info@programarivm.com>
2023-10-30 12:09:21 +0100

    Improved styles

--- src/OpeningTable.js
+++ src/OpeningTable.js
@@ -1,16 +1,16 @@
 import { Ascii } from './common/Ascii';
+import styles from './styles/openingTable';
 
 export const OpeningTable = ({ stateOpeningTable }) => {
   if (stateOpeningTable.fen[0] === Ascii.initialFen()) {
     if (stateOpeningTable.opening) {
       return (
-        <table>
+        <table style={styles.table}>
           <tbody>
             {
               stateOpeningTable.opening.rows?.map((item, i) => (
-                <tr key={i}>
-                  <td align="left">{item.eco}</td>
-                  <td align="left">{item.name}</td>
+                <tr key={i} style={styles.tr}>
+                  <td style={styles.td}>{item.eco} {item.name}</td>
                 </tr>
               ))
             }
---
+++ src/styles/openingTable.js
@@ -0,0 +1,16 @@
+const styles = {
+  table: {
+    width: '100%',
+  },
+  td: {
+    fontSize: "0.875rem",
+    lineHeight: 1.43,
+    textAlign: "center",
+    padding: "6px 16px",
+  },
+  tr: {
+    height: '2.25rem',
+  },
+}
+
+export default styles;
